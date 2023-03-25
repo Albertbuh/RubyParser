bb167ba953714de0ccfbeb7b16f14e4b8676b180
albertbuh <albertbuhovets@mail.ru>
2023-03-25 23:07:32 +0300

    push 'puts' file'

---
+++ inter/Statements/Puts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RubyParser.inter.Statements
+{
+    public class Puts : Stmt
+    {
+        Expr x;
+        String str;
+        public Puts(Expr x)
+        {
+            this.x = x;
+            str = "";
+        }
+        public Puts(string str)
+        {
+            this.str = str;
+            this.x = null;
+        }
+
+        public override void Gen(int b, int a)
+        {
+            if (x != null)
+                Emit("puts " + x.ToString());
+            else
+                Emit("puts " + str);
+        }
+
+
+    }
+}
